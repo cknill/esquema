d43ddd673937c2b522396bfba615f39e18ab53f1
cknill <knill.christopher@gmail.com>
2024-08-26 14:39:35 -0600

    Added error handling to the top-level and added ':q' as a convenient way to quit the REPL

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,17 @@
 #include "interp.hh"
 #include "linenoise.hpp"
+#include <exception>
 #include <iostream>
 #include <string>
 
 namespace {
+    using namespace std::literals::string_view_literals;
     using namespace std::literals::string_literals;
+    using namespace esquema;
 }
 
 int main(int argc, char ** argv) {
-    esquema::Interpreter interpreter{};
+    Interpreter interpreter{};
     std::string line;
     while (true) {
         auto quit = linenoise::Readline("esquema> ", line);
@@ -16,7 +19,26 @@ int main(int argc, char ** argv) {
             break;
         }
 
-        std::cout << interpreter.eval(line) << '\n';
+        if (line.empty()) {
+            continue;
+        }
+        // TODO - This is like Haskell's REPL
+        // Scheme might do it some other way.
+        else if (line == ":q"sv) {
+            break;
+        }
+
+        try {
+            std::cout << interpreter.eval(line) << '\n';
+        }
+
+        catch (std::exception const & ex) {
+            std::cerr << ex.what() << '\n';
+        }
+        catch (...) {
+            std::cerr << "Unexpected error, exiting\n"sv;
+            return EXIT_FAILURE;
+        }
     }
 
     return EXIT_SUCCESS;
